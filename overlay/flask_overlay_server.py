04b686de0861723683cfd105ff6281c6a5032598
nickdnj <nickd@demarconet.com>
2025-06-25 15:17:07 -0400

    Change Flask overlay server port to 5050

--- overlay/flask_overlay_server.py
+++ overlay/flask_overlay_server.py
@@ -30,4 +30,4 @@ def overlay():
     return send_file(img_bytes, mimetype='image/png')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000) 
+    app.run(host='0.0.0.0', port=5050) 
